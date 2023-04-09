26dd024f83de8e1c7322db9aaf9af9e54b789a38
Rash-i <sierrahuaracha_xd@hotmail.com>
2023-04-09 00:13:03 -0500

    uuu

---
+++ Estudio.Practica/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std; 
+int main(){
+    int h, c1 ,c2;
+    
+
+
+    cout<<"Ingrese el valor de la hipotenusa: ";
+    cin>>h;
+    cout<<"Ingrese el valor del cateto 1: ";
+    cin>> c1;
+    cout<<"Ingrese el valor del cateto 2: ";
+    cin>> c2;
+    
+
+
+    return 0;
+}
--- Semana4/1.cpp
+++ Semana4/1.cpp
@@ -9,7 +9,7 @@ int main(){
     cin >> n;
 
     int n1, n2, fib;
-    n1 = n2 = 1
+    n1 = n2 = 1;
     if(n == 1 || n == 1)
         cout <<"El fib de "<< n << "es "<< 1<<endl;
     else{
