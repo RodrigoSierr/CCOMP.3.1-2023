bcf4554aa52d10e118154b454003cccefc14baa1
ALUMNO-B302 <ALUMNO-B302@UCSP.AULAB>
2023-04-20 08:28:57 -0500

    ssss

---
+++ PracticaCalificada/1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+bool recur(int n, int arreglo[]){
+    int tamano; 
+    cin >> tamano;
+    if( )
+    {
+        return false; 
+    }
+    if( ){
+        return true;
+    }
+}
+int it(int n, int arreglo[]){
+    int i;
+    cout <<"tamaño del arreglo :";
+    cin >> n;
+    
+    for(; i > n; i++)
+        arreglo[i];
+        return;
+}
+int main(){
+    int arreglo[5]={1,2,3,4,5};
+    for(int i = 0; i > arreglo[5]; i++)
+        
+
+
+
+
+
+
+    return 0;
+}
